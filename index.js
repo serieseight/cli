c722879b2e0b162ce0d224e8abecc732bcb1d970
Colin Meinke <hello@colinmeinke.com>
2018-09-22 09:04:34 +0100

    feat(version): add version flag

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 
 const chalk = require('chalk')
 const helloCmd = require('./commands/hello')
+const package = require('./package.json');
 
 const args = process.argv.splice(2)
 
 switch(args[0]) {
+  case '--version':
+  case '-v':
+    console.log(package.version)
+    break
+
   case 'hello':
     helloCmd(args)
     break
